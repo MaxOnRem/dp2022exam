eec7f7305e76c08de3d5318c77da815c7656dc53
MaxRem <72015150+MxRem@users.noreply.github.com>
2022-06-14 12:47:16 +0300

    Update user11.sql

--- DataBase/user11.sql
+++ DataBase/user11.sql
@@ -1,4 +1,9 @@
+create database user11;
+create user andrii_miroshnyk with password '121';
+grant all on database data7 to user11;
+
 CREATE TABLE "data7" (
+  
   "id" serial PRIMARY KEY,
   "title11" varchar,
   "age" bigint
