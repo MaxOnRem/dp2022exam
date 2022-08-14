47042e40868d4942887235aaedd20776fc5ffee3
MaxRem <72015150+MxRem@users.noreply.github.com>
2022-08-14 23:59:09 +0300

    Update user11.sql

--- DataBase/user11.sql
+++ DataBase/user11.sql
@@ -1,5 +1,5 @@
 create database user11;
-create user andrii_miroshnyk with password '121';
+create user max_rem with password '121';
 grant all on database data7 to user11;
 
 CREATE TABLE "data7" (
